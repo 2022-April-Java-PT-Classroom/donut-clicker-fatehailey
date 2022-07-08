859939e749aa007282d2bad9ef63843c8b5a12a5
fatehailey <fatehailey@yahoo.com>
2022-07-07 21:02:58 -0400

    added info abt baker unsure why link on home to get to baking.html doesnt work

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-import DonutMaker from "./DonutMaker";
-
-
 function onClickMenu(){
   document.getElementById("menu").classList.toggle("icon");
   document.getElementById("nav").classList.toggle("change");
