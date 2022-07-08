2aeeda86ee1df595f5c75fea2a75ba602e4ec9d9
fatehailey <fatehailey@yahoo.com>
2022-07-08 19:35:35 -0400

    cant seem to get my buttons to work..

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
+import DonutMaker from "DonutMaker.js";
+
 function onClickMenu(){
   document.getElementById("menu").classList.toggle("icon");
   document.getElementById("nav").classList.toggle("change");
 }
 
+
+
+
+  const donutAmount = document.querySelector('#donutCount');
+  const totalDonuts = document.querySelector('#donutClicker');
+  const totalAuto = document.querySelector('#totalAutoClickers') ;
+  const autoButton = document.querySelector('#addClicker');
+  const totalMultiplier = document.querySelector('#totalMultipliers');
+  const multiplierButton = document.querySelector('#addMultiplier');
+  const valuePerClick = document.querySelector('#perClick');
+  const resetButton = document.querySelector('#resetButton');
+
+  DonutMaker = new DonutMaker(0, 0, 100, 0, 10);
+
+donutAmount.addEventListener("click", () => {
+  DonutMaker.addDonut();
+  DonutMaker.activateMultipliers();
+  numDonutsDiv.innerHTML = DonutMaker.numDonuts;
+});
